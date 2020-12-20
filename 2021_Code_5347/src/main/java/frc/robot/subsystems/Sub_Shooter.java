b1e05208de9c6618ea158a838da80310114af2cb
Ben Laster <benlaster@Bens-MacBook-Pro.local>
2020-12-20 14:16:33 -0500

    Imported Pheonix

--- 2021_Code_5347/src/main/java/frc/robot/subsystems/Sub_Shooter.java
+++ 2021_Code_5347/src/main/java/frc/robot/subsystems/Sub_Shooter.java
@@ -10,6 +10,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 
 public class Sub_Shooter extends SubsystemBase {
